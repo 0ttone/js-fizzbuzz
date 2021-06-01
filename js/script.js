3da771557a26d24f0a49069d945e094d994f0921
MyUsername <emanuele.marini37@gmail.com>
2021-06-01 17:04:15 +0200

    prima stesura ok 100

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Scrivi un programma che stampi i numeri da 1 a 100
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. 
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+
+//definizione variabili
+
+var Fizz = "Fizz";
+var Buzz = "Buzz";
+var FizzBuzz = "FizzBuzz"
+var output = document.getElementById("output")
+var list
+
+//definizione ciclo output da 1 a 100 con for
+
+for (var i = 1; i <=100; i++){
+      output.innerHTML += "</br> " + i;
+}
+
